cd493d154cebd57f375923a88e6b8bf514e2bc7e
root <root@garibat.garibat>
2026-02-02 19:18:23 +0300

    salam

--- linux_security/0x01_permissions_sguid_sgid/6-check_files.sh
+++ linux_security/0x01_permissions_sguid_sgid/6-check_files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find $1 -type f \( -perm -4000 -o -perm -2000 \) -mtime -1 -print0 | xargs -0 ls -l
+find "$1" -type f -mtime -1 \( -perm -4000 -o -perm -2000 \) -exec ls -l {} \; 2>/dev/null
